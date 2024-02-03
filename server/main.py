f5b26943ac66a35b9053a63ab049b3bde9921ac7
jakejasminjk <jakejasminjk@gmail.com>
2024-02-03 13:30:57 -0500

    initial folders

---
+++ server/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import os
+import ast
+import json
+import ssl
+from flask_cors import CORS
+from apiCalls import summarize_transcript
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route("/", methods=['GET'])
+def home():
+    return "PSREAD API"
+
+
+@app.route("/", methods=['POST'])
+def summarize_text():
+    data = request.get_json()
+    text_transcript = data["text"]
+    return summarize_transcript(text_transcript)
+
+
+if __name__ == '__main__':
+    app.run()
