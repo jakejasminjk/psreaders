c242b280c69b477895d4a88d0bf22a97e3e7ddb1
Ka Wong <kawong2@WKMZTF6825A4.global.publicisgroupe.net>
2024-02-03 17:53:32 -0600

    add starting summary and speed section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Example from "./components/Example";
 import "./style.css";
 import { Typography } from "@material-tailwind/react";
 import TeachingStyle from "./components/TeachingStyle";
+import Summary from "./components/Summary";
+import SPEED from "./components/SPEED";
 
 const App = () => {
   return (
@@ -11,12 +12,11 @@ const App = () => {
         <Typography variant="h2">PS Reader</Typography>
         <TeachingStyle />
       </div>
-      <div className="grid grid-cols-3">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
+      <div className="grid grid-cols-3 gap-3">
+        <Summary />
+        <SPEED />
+        <Summary />
       </div>
-      <Example />
     </div>
   );
 };
---
+++ client/src/components/SPEED.tsx
@@ -0,0 +1,26 @@
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+import React from "react";
+
+const SPEED = () => {
+  return (
+    <Card className="">
+      <CardBody>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+          SPEED
+        </Typography>
+        <Typography>
+          The instructor discusses the importance of having a standard template
+          for defining test cases in software projects. They emphasize...
+        </Typography>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default SPEED;
