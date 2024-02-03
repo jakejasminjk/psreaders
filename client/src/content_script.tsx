56a81b8a37d3eec8fa42ee8b43b2f9ada09331bd
Ka Wong <kawong2@WKMZTF6825A4.global.publicisgroupe.net>
2024-02-03 14:13:50 -0600

    setup init chrome extension

---
+++ client/src/content_script.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Example from "./components/Example";
+import "./index.css";
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  if (msg.color) {
+    console.log("Receive color = " + msg.color);
+    document.body.style.backgroundColor = msg.color;
+    sendResponse("Change color to " + msg.color);
+  } else {
+    sendResponse("Color message is none.");
+  }
+});
+
+const parentNode = document.getElementsByClassName(
+  "paid-course-landing-page__body"
+);
+
+if (parentNode) {
+  const targetNode = document.createElement("div");
+  targetNode.id = "targetDiv";
+  parentNode[0].insertBefore(targetNode, parentNode[0].firstChild);
+
+  const root = createRoot(targetNode);
+
+  root.render(
+    <React.StrictMode>
+      <Example />
+    </React.StrictMode>
+  );
+}
